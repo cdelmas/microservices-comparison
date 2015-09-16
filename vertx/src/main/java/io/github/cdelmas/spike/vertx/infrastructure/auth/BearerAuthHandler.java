0095b630b9952f4975de33f1fa77332569ca3caa
cdelmas <cyril.delmas@gmail.com>
2015-09-16 13:42:32 +0200

    Add License header where missing.

--- vertx/src/main/java/io/github/cdelmas/spike/vertx/infrastructure/auth/BearerAuthHandler.java
+++ vertx/src/main/java/io/github/cdelmas/spike/vertx/infrastructure/auth/BearerAuthHandler.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2015 Cyril Delmas
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package io.github.cdelmas.spike.vertx.infrastructure.auth;
 
 import io.vertx.core.http.HttpHeaders;
@@ -7,10 +22,6 @@ import io.vertx.ext.auth.AuthProvider;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.impl.AuthHandlerImpl;
 
-/**
- * (c) Lectra.
- * @author c.delmas
- */
 public class BearerAuthHandler extends AuthHandlerImpl {
 
     public BearerAuthHandler(AuthProvider authProvider) {
