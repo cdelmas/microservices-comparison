200005fee352745a339fa0d3d35432cdebde9c1a
cdelmas <cyril.delmas@gmail.com>
2015-09-24 13:13:03 +0200

    Some further functions.
    Fix build for spring boot.
    Add Unirest to call resource.
    Configure the object mapper properly.

--- sparkjava/src/main/java/io/github/cdelmas/spike/sparkjava/Main.java
+++ sparkjava/src/main/java/io/github/cdelmas/spike/sparkjava/Main.java
@@ -15,14 +15,19 @@
 */
 package io.github.cdelmas.spike.sparkjava;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 import io.github.cdelmas.spike.common.domain.Car;
 import io.github.cdelmas.spike.common.domain.CarRepository;
 import io.github.cdelmas.spike.common.hateoas.Link;
 import io.github.cdelmas.spike.common.persistence.InMemoryCarRepository;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Arrays;
 import java.util.List;
 
 import static io.github.cdelmas.spike.common.hateoas.Link.self;
@@ -34,10 +39,41 @@ import static spark.SparkBase.port;
 public class Main {
     public static void main(String[] args) {
         port(8099); // defaults on 4567
-        get("/hello", (request, response) -> "Hello World");
-
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        Unirest.setObjectMapper(new com.mashape.unirest.http.ObjectMapper() {
+            @Override
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return objectMapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+
+            @Override
+            public String writeValue(Object value) {
+                try {
+                    return objectMapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        get("/hello", (request, response) -> {
+                    HttpResponse<Car[]> carHttpResponse = Unirest.get("http://localhost:8099/cars")
+                            .header("Accept", "application/json")
+                            .asObject(Car[].class);
+                    Car[] cars = carHttpResponse.getBody();
+                    List<String> carNames = Arrays.stream(cars)
+                            .map(Car::getName)
+                            .collect(toList());
+                    return "We have these cars available: " + carNames;
+                }
+        );
+
 
         CarRepository carRepository = new InMemoryCarRepository();
         get("/cars", "application/json", (request, response) -> {
@@ -58,6 +94,7 @@ public class Main {
                 car = objectMapper.readValue(request.body(), Car.class);
                 carRepository.save(car);
                 response.header("Location", request.url() + "/" + car.getId());
+                response.status(201);
             } catch (IOException e) {
                 response.status(400);
             }
